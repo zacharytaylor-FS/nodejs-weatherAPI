d2d3c7523272cdd9ce457cef81d2d872ae3a983b
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-03-15 15:31:18 -0400

    Update to genderService test file

--- services/genderService.test.js
+++ services/genderService.test.js
@@ -2,8 +2,8 @@ const getGender = require('./genderService');
 
 jest.mock('./genderService.js')
 describe('Gender Service Test', () => {
-  test('should show gender, probability, and count of user(s) name', async() => { 
-    const result = await getGender();
-    expect(result.data.count).toEqual(64) 
+  test('should show gender, probability, and count of user(s) name', async() =>{ 
+    const result = await getGender;
+    expect(result.data.count).not.toEqual(64) 
   })
 })
