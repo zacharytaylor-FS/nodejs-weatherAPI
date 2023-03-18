b82fea56f36f9df5fe9da39662b3cdf8c1e9f58d
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-03-18 17:25:19 -0400

    Update NodeJs log

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app/app');
 require('dotenv').config();
 
 http.createServer(app).listen(process.env.PORT, () => {
-  console.log(`Server running on port: ${process.env.PORT}`)
+  console.log(`NodeJS Server running on port: ${process.env.PORT}`)
 })
