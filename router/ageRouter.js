dc9e5d028e70d66f354c9250a852966f164097a6
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-03-19 21:48:20 -0400

    Age Service test  along with Router added

---
+++ router/ageRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const getAge = require('../services/ageService');
+const ageRouter = express.Router();
+
+ageRouter.get('/', (req, res, next) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  getAge(name)
+    .then(result => {
+      console.log(result.data);
+      res.status(200).json({
+        name: req.body.name,
+        age: result.data.age
+      })
+    }).catch(error => {
+      res.status(501).json({
+        error: {
+          message: error.message,
+          status: error.status
+        }
+      })
+    });
+})
+
+module.exports = ageRouter
--- services/ageService.test.js
+++ services/ageService.test.js
@@ -1,9 +1,10 @@
 const getAge = require('./ageService');
 
 //describe, test, expect
+jest.mock('./ageService.js')
 describe('Age Service Test: ', () => { 
   test('should take in a user(s) name ', async () => { 
     const result = await getAge;
-    expect(result.data)
+    expect(result.data.info[results]).toBe(200)
   }) 
 })
