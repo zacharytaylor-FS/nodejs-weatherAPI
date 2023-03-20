8d0e03ac8a2d538bd7c1caea91594389773f2ae2
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-03-19 21:52:06 -0400

    User Service along with testing  added

---
+++ services/userService.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const getUser = async() => {
+  console.log('Real Random User Service');
+  return await axios.get(`${process.env.randomUSERURL}`)
+}
+
+module.exports = getUser
